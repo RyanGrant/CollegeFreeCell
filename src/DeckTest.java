f440e6e8ed79b68710c15f4ab459105a916914d8
RyanGrant <grantr16@mail.wlu.edu>
2017-12-02 17:07:57 -0500

    Added a couple of basic unit tests

---
+++ src/DeckTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class DeckTest {
+	Deck d = new Deck();
+	@Test
+	public void testDeck() {
+		Deck d = new Deck();
+		assertNotNull(d);
+	}
+
+	@Test
+	public void testIsEmpty() {
+		Deck d = new Deck();
+		for (int i = 0; i < 52; i++) {
+			d.deal();
+		}
+		assertTrue(d.isEmpty());
+	}
+
+	@Test
+	public void testDeal() {
+		Deck d = new Deck();
+		Card actual = d.deal();
+		assertEquals(13, actual.getRank());
+	}
+}
